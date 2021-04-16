8c67e7181efd552b750114dbf983663a767209c8
Denis N <service4tv@gmail.com>
2021-04-16 23:13:06 +0300

    Add project files.

---
+++ Triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleInfo
+{
+    public class TriangleInfo<T> : ITriangle
+    {
+        public T LongSide { get;  set; }
+
+        public T SecondSide { get; set; }
+
+        public T ThirdSide { get; set; }
+
+        private TriangleService<T> validationService;
+
+        public TriangleInfo(T firstSide, T secondSide, T thirdSide)
+        {
+           validationService = new TriangleService<T>(firstSide, secondSide, thirdSide);
+           validationService.CheckForValueType();
+            LongSide = firstSide;
+            SecondSide = secondSide;
+            ThirdSide = thirdSide;
+
+        }
+        public string GetTriangleType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
